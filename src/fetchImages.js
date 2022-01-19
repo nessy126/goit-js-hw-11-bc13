77526d18c40e503607d1709f65134e4c868c857b
nessy126 <nessy126@gmail.com>
2022-01-19 11:31:35 +0300

    Update fetchImages.js

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,12 +1,13 @@
-// const axios = require('axios').default;
+const axios = require('axios').default;
 
 export default function fetchImages(q, page = 1, perPage = 40, orientation = 'horizontal') {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY_API = '25273711-e95d23ce228d19c821de1faa8';
 
-  return fetch(
+  return axios.get(
     `${BASE_URL}?key=${KEY_API}&q=${q}&image_type=photo&orientation=${orientation}&safesearch=true&page=${page}&per_page=${perPage}`,
   )
-  .then(data => data.json());
+    .then(response => console.log(response.data))
+    .catch(err => console.log(err))
   //axios#get(url[, config])
 }
