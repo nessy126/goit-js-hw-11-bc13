a2fc039ca5b0abc8432e63b75caecae9b4fa19d0
nessy126 <nessy126@gmail.com>
2022-01-19 14:57:10 +0300

    fff

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,13 +1,23 @@
-const axios = require('axios').default;
+const axios = require('axios');
 
-export default function fetchImages(q, page = 1, perPage = 40, orientation = 'horizontal') {
+export default async function fetchImages(q, page = 1, perPage = 40, orientation = 'horizontal') {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY_API = '25273711-e95d23ce228d19c821de1faa8';
+  const generalURL =  `${BASE_URL}?key=${KEY_API}&q=${q}&image_type=photo&orientation=${orientation}&safesearch=true&page=${page}&per_page=${perPage}`
 
-  return axios.get(
-    `${BASE_URL}?key=${KEY_API}&q=${q}&image_type=photo&orientation=${orientation}&safesearch=true&page=${page}&per_page=${perPage}`,
-  )
-    .then(response => console.log(response.data))
-    .catch(err => console.log(err))
+  // const requestParams = {
+  //   key: KEY_API,
+  //   q: q,
+  //   image_type: 'photo',
+  //   orientation: orientation,
+  //   safesearch: true,
+  //   page,
+  //   per_page: perPage,
+  //       }
+  const data = await axios.get(generalURL);
+  // console.log(data)
+  return data.data;
+    // .then(response => console.log(response.data))
+    // .catch(err => console.log(err));
   //axios#get(url[, config])
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import './sass/main.scss';
-
 import fetchImages from './fetchImages';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // const InfiniteScroll = require('infinite-scroll');
 
 const formEl = document.querySelector('form');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-loadMoreBtn.disabled = true;
 const orientation = 'horizontal';
 let page = 1;
 const perPage = 40;
-const q = formEl.elements.searchQuery.value;
-
+let q;
 
 const renderMarkup = data => {
-  console.log(data)
-      const markup = data.hits
-        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-  `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  console.log(data);
+  const markup = data.hits
+    .map(
+      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+        `<div class="photo-card">
+  <a href="${webformatURL}">
+  <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>${likes}
@@ -33,51 +36,69 @@ const renderMarkup = data => {
       <b>Downloads</b>${downloads}
     </p>
   </div>
-  </div>`)
-        .join();
+  </a>
+  </div>`,
+    )
+    .join();
   galleryEl.insertAdjacentHTML('beforeend', markup);
-  loadMoreBtn.disabled = false;
-}
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+};
 
-const onLoad = async (e) => {
+
+const onLoad = async e => {
   e.preventDefault();
-  page = 500;
+  page = 1;
+  galleryEl.innerHTML = '';
+  q = formEl.elements.searchQuery.value;
   const data = await fetchImages(q, page, perPage, orientation);
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images` );
   if (data.length === 0) {
-    alert('Sorry, there are no images matching your search query. Please try again.');
-    return
-  }
-  console.log(data)
-  renderMarkup(data)
-}
-
-const onLoadMore = async () => {
-  const data = await fetchImages(q, ++page, perPage, orientation);
-  if (data.length === 0) {
-    alert('Sorry, there are no images matching your search query. Please try again.');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+        loadMoreBtn.style.display = 'none';
     return;
   }
+  loadMoreBtn.style.display = 'flex';
+  console.log(data);
+  renderMarkup(data);
+};
+
+const onLoadMore = async () => {
+  q = formEl.elements.searchQuery.value;
+  const data = await fetchImages(q, ++page, perPage, orientation);
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+          loadMoreBtn.style.display = 'none';
+    return;
+  }
+
+  if (data.hits.length < 40) {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }
     renderMarkup(data);
-  ;
-}
+};
 
 formEl.addEventListener('submit', onLoad);
-loadMoreBtn.addEventListener('click', onLoadMore)
+loadMoreBtn.addEventListener('click', onLoadMore);
 
-window.addEventListener('scroll', function (e) {
-  console.log(window.pageYOffset); //текущее положение скрола
-  console.log(galleryEl.offsetHeight); //высота гэлЭл
+// window.addEventListener('scroll', function (e) {
+//   // console.log(window.pageYOffset); //текущее положение скрола
+//   // console.log(galleryEl.offsetHeight); //высота гэлЭл
 
-  const contentHeight = block.offsetHeight; // 1) высота блока контента вместе с границами
-  const yOffset = window.pageYOffset; // 2) текущее положение скролбара
-  const window_height = window.innerHeight; // 3) высота внутренней области окна документа
-  const y = yOffset + window_height;
+//   const contentHeight = block.offsetHeight; // 1) высота блока контента вместе с границами
+//   const yOffset = window.pageYOffset; // 2) текущее положение скролбара
+//   const window_height = window.innerHeight; // 3) высота внутренней области окна документа
+//   const y = yOffset + window_height;
 
-  if (y >= contentHeight)  onLoadMore();
-  // если пользователь достиг конца
+//   if (y >= contentHeight) onLoadMore();
+//   // если пользователь достиг конца
 
-  // if (window.pageYOffset - 300 <= galleryEl.offsetHeight)
-});
+//   // if (window.pageYOffset - 300 <= galleryEl.offsetHeight)
+// });
 
 // function onScroll(q, orientation, page = 1, perPage) {}
 
